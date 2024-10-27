60743f8ce1811ed65ccd5ba6190a660a4125d55c
ppshobish <shobish@zingmyorder.com>
2024-10-27 20:54:43 +0530

    finished

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ menuOpenButton.addEventListener('click',()=>{
     document.body.classList.toggle('show-mobile-menu');
 });
 menuCloseButton.addEventListener('click',()=>menuOpenButton.click());
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    grabCursor:true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+   breakpoints:{
+    0:{
+        slidesPerView:1
+    },
+    768:{
+        slidesPerView:2
+
+    },
+    1024:{
+        slidesPerView:3
+          
+    }
+
+   }
+   
+  });
